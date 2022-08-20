df8810ccef77b96c23c3bcb0ef18caf1917e0dff
Ali KORKMAZ <ali.korkmaz@paycore.com>
2022-08-20 19:18:47 +0300

    Added Components ProductList and Card

---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,17 @@
+import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
+import { Product } from "../../app/models/product";
+
+interface Props {
+    product:Product;
+}
+export default function ProuctCard({product}:Props){
+    return (
+         <ListItem key={product.id}>
+        <ListItemAvatar>
+            <Avatar src={product.pictureUrl}></Avatar>
+        </ListItemAvatar>
+        <ListItemText>{product.name}- {product.price}</ListItemText>
+      </ListItem>
+    )
+
+}
