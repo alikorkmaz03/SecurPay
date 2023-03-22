c782a1052c6d81837d3cd581dd7883452ff52ae2
Ali Korkmaz <alikorkmaz03@gmail.com>
2023-03-22 03:22:01 +0300

    Orders operations added and created OrderExtensions

---
+++ API/DTO/OrderDto.cs
@@ -0,0 +1,27 @@
+﻿using API.Entities.BulkOrder;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+
+        public string BuyerId { get; set; }
+
+        [Required]
+        public ShippingAddress ShippingAddress { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        public List<OrderItemDto> OrderItems { get; set; }
+
+        public long Subtotal { get; set; }
+
+        public long DeliveryFee { get; set; }
+
+        public string OrderStatus { get; set; }
+
+        public long Total { get; set; }
+    }
+}
---
+++ API/Extensions/OrderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using API.DTO;
+using API.Entities.BulkOrder;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace API.Extensions
+{
+    public static class OrderExtensions
+    {
+        public static IQueryable<OrderDto>ProjectOrderToOrderDto(this IQueryable<Order> query)
+        {
+            return query
+                .Select(order => new OrderDto
+                {
+                    Id = order.Id,
+                    BuyerId = order.BuyerId,
+                    OrderDate = order.OrderDate,
+                    ShippingAddress = order.ShippingAddress,
+                    DeliveryFee = order.DeliveryFee,
+                    Subtotal = order.Subtotal,
+                    OrderStatus = order.OrderStatus.ToString(),
+                    Total = order.GetTotal(),
+                    OrderItems = order.OrderItems.Select(item => new OrderItemDto
+                    {
+                        ProductId = item.ItemOrdered.ProductId,
+                        Name = item.ItemOrdered.Name,
+                        PictureUrl = item.ItemOrdered.PictureUrl,
+                        Price = item.Price,
+                        Quantity = item.Quantity,
+                    }).ToList()
+
+                }).AsNoTracking();
+
+            //"AsNoTracking" özelliği, verilerin sadece okunacağı durumlarda kullanılmalıdır. Çünkü bu özellik, bir varlık nesnesinin değişikliklerinin takip edilememesi anlamına gelir.
+            //Bu nedenle, bir varlık nesnesinin güncellenmesi gerekiyorsa, "AsNoTracking" özelliği kullanılmamalıdır. Aksi takdirde, varlık nesnesi değiştirilemeyeceği için hatalar meydana gelebilir.
+        }
+    }
+}
