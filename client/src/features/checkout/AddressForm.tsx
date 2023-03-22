f1f900e4153c12f55acc4d4f699ca344f7ed2a04
Ali Korkmaz <alikorkmaz03@gmail.com>
2023-03-22 05:26:00 +0300

    added checkout operations

---
+++ client/src/features/checkout/AddressForm.tsx
@@ -0,0 +1,45 @@
+﻿import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { useFormContext } from 'react-hook-form';
+import AppTextInput from '../../app/components/AppTextInput';
+import AppCheckbox from '../../app/components/AppCheckbox';
+
+export default function AddressForm() {
+    const { control } = useFormContext();
+    return (
+        <>
+            <Typography variant="h6" gutterBottom>
+                Kargo Adresi
+            </Typography>
+
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='fullName' label='Ad Soyad' />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <AppTextInput control={control} name='address1' label='Addres Satır 1' />
+                </Grid>
+                <Grid item xs={12}>
+                    <AppTextInput control={control} name='address2' label='Addres Satır 2' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='city' label='City' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='state' label='İlçe' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='zip' label='Posta Kodu' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control} name='country' label='Ülke' />
+                </Grid>
+                <Grid item xs={12}>
+                    <AppCheckbox name='saveAddress' label='Varsayılan adres olarak Kaydet' control={control} />
+                </Grid>
+            </Grid>
+
+        </>
+    );
+}
---
+++ client/src/features/checkout/checkoutValidation.ts
@@ -0,0 +1,12 @@
+import * as yup from 'yup';
+
+export const validationSchema = yup.object({
+
+    fullName: yup.string().required('Ad Soyad bilginizi girmelisiniz...'),
+    address1: yup.string().required('Address bilginizi girmelisiniz...'),
+    address2: yup.string().required(),
+    city: yup.string().required(),
+    state: yup.string().required(),
+    zip: yup.string().required(),
+    country: yup.string().required()
+})
